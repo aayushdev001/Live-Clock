5c6b6fe2822a0f93ee9b8408bb52b4dbfbadceca
Aayush Nag <97802989+blasphemycentral@users.noreply.github.com>
2022-03-20 22:49:43 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,27 @@
+setInterval(() => {
+
+        let time = document.getElementById("p1");
+        let date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        time.textContent = hours + ":" + minutes + ":" + seconds;
+    }, 1000);
+
+    setInterval(() =>{
+        bg = document.querySelector("body");
+        bg.style.backgroundImage = "url(https://source.unsplash.com/1600x1200?india)";
+        // document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x1200?india')";
+        // document.body.style.backgroundImage = "url('./img/London, United Kingdom 3l1sjp562qQ.jpg')";
+        console.log("Chal be");
+        // bg.style.backgroundColor = "red";
+    },3000);
